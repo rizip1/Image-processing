d2a274584e1b79e28062f2aaa588cb7d64669119
richard <richardizip@gmail.com>
2016-01-28 18:58:54 +0100

    update: implemented p_hash
    * also resized kernels from 8*8 to 16*16
      for higher precision

--- functions.py
+++ functions.py
@@ -1,11 +1,39 @@
 import cv2
+import numpy as np
+
+
+def p_hash(img):
+    big_kernel = 64
+    small_kernel = int(big_kernel / 4)
+
+    img = _get_shrinked_grayscale(img, big_kernel, big_kernel)
+    img = np.float32(img) / 255.0 
+    img = cv2.dct(img)
+    
+    average = 0
+    for i in range(small_kernel):
+        for j in range(small_kernel):
+            average += img[i][j]
+    
+    average -= img[0][0]
+    average /= ((small_kernel**2) - 1)
+
+    hash_string = []
+    for i in range(small_kernel):
+         for j in range(small_kernel):
+            if (img[i][j] < average):
+                hash_string.append('1')
+            else:
+                hash_string.append('0')
+
+    return ''.join(hash_string)
 
 
 def a_hash(img):
     '''
     Creates aHash string from given image
     '''
-    img = _get_shrinked_grayscale(img, 8, 8)
+    img = _get_shrinked_grayscale(img, 16, 16)
     average = _get_intensity_average(img)
     hash_string = []
 
@@ -23,7 +51,7 @@ def d_hash(img):
     '''
     Creates dHash string from given image.
     '''
-    img = _get_shrinked_grayscale(img, 9, 8)
+    img = _get_shrinked_grayscale(img, 17, 16)
     hash_string = []
 
     for i in range(len(img)):
@@ -80,5 +108,6 @@ if __name__ == "__main__":
     pic1 = cv2.imread('src/sample1.jpg')
     pic2 = cv2.imread('src/sample2.jpg')
     print(compare_hashes(a_hash(pic1), a_hash(pic2)))
-    print(compare_hashes(d_hash(pic1), d_hash(pic2)))
+    print(compare_hashes(d_hash(pic1), d_hash(pic2)))    
+    print(compare_hashes(p_hash(pic1), p_hash(pic2)))
 
